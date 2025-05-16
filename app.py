2ef0c021ca8bd567c352cfd9e49cdf68e3c49e0a
MD ADNAN <76099117+adnan8914@users.noreply.github.com>
2025-05-16 16:00:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,9 +7,10 @@ app = Flask(__name__)
 def home():
     return jsonify({"message": "Flask is running"})
 
-@app.route('/time')
+@app.route('/time', methods=['GET', 'POST'])
 def get_time():
     now = datetime.utcnow()
     return jsonify({
         "message": f"Today is {now.strftime('%A')}, and the current time is {now.strftime('%H:%M:%S UTC')}"
     })
+
